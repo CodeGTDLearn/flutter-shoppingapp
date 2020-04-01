7f29ce794e9f3f46a76498e1d2fb9dd98d879350
PauloPortfolio <pvba02@gmail.com>
2020-04-01 14:07:30 -0600

    ShopCart basic structure done

--- lib/views/cart_view.dart
+++ lib/views/cart_view.dart
@@ -7,28 +7,35 @@ import 'package:shopingapp/config/titlesIcons.dart';
 import 'package:shopingapp/widgets/badge.dart';
 
 class CartView extends StatelessWidget {
-
-  final _servStore = Modular.get<IItemsOverviewGridProductsStore>();
-
+  final _servGridProductsStore = Modular.get<IItemsOverviewGridProductsStore>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(CRT_APPBAR_TITLE),
-        actions: [
-          PopupMenuButton(
+      appBar: AppBar(title: Text(CRT_APPBAR_TITLE), actions: [
+        PopupMenuButton(
             itemBuilder: (_) => [
-              PopupMenuItem(
-                child: Text(IOS_TXT_POPUP_FAV),
-                value: ItemsOverviewPopup.Favorites,
-                enabled: _servStore.totalFavoritesQtde() != 0 ? true : false,
-              ),
-              PopupMenuItem(child: Text(IOS_TXT_POPUP_ALL), value: ItemsOverviewPopup.All)
-            ],
+                  PopupMenuItem(
+                    child: Text(IOS_TXT_POPUP_FAV),
+                    value: ItemsOverviewPopup.Favorites,
+                    enabled: _servGridProductsStore.totalFavoritesQtde() != 0 ? true : false,
+                  ),
+                  PopupMenuItem(child: Text(IOS_TXT_POPUP_ALL), value: ItemsOverviewPopup.All)
+                ],
             onSelected: (popupOptionSelected) =>
-                _servStore.applyFilter(popupOptionSelected, context),
-          ),
+                _servGridProductsStore.applyFilter(popupOptionSelected, context))
+      ]),
+      body: Column(
+        children: [
+          Card(
+              child: Row(children: <Widget>[
+            Text(CRT_TXT_CARD),
+            Chip(label: null),
+            Spacer(),
+            FlatButton(onPressed: null, child: null)
+          ])),
+          SizedBox(height: 10),
+          Expanded()
 
         ],
       ),
