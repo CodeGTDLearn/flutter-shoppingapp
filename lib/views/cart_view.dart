ef933cb6b87bf26f9cc4d356ba9c35662833a629
PauloPortfolio <pvba02@gmail.com>
2020-04-01 12:36:10 -0600

    working in the cart

---
+++ lib/views/cart_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:shopingapp/enum/itemOverviewPopup.dart';
+import 'package:shopingapp/service_stores/ItemsOverviewGridProductsStore.dart';
+import 'package:shopingapp/config/appProperties.dart';
+import 'package:shopingapp/config/titlesIcons.dart';
+import 'package:shopingapp/widgets/badge.dart';
+
+class CartView extends StatelessWidget {
+  final _servStore = Modular.get<IItemsOverviewGridProductsStore>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(CRT_APPBAR_TITLE),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text(IOS_TXT_POPUP_FAV),
+                value: ItemsOverviewPopup.Favorites,
+                enabled: _servStore.totalFavoritesQtde() != 0 ? true : false,
+              ),
+              PopupMenuItem(child: Text(IOS_TXT_POPUP_ALL), value: ItemsOverviewPopup.All)
+            ],
+            onSelected: (popupOptionSelected) =>
+                _servStore.applyFilter(popupOptionSelected, context),
+          ),
+        ],
+      ),
+    );
+  }
+}
