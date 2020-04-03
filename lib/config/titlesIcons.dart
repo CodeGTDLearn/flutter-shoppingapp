ddc5b9f58ce6ab03cd8c937efde0c5b95d1df120
PauloPortfolio <pvba02@gmail.com>
2020-04-03 14:16:18 -0600

    AppbarPopupMenu refactored

--- lib/config/titlesIcons.dart
+++ lib/config/titlesIcons.dart
@@ -8,7 +8,7 @@ const IOS_ICO_SHOP = Icon(Icons.shopping_cart);
 const IOS_ICO_FAV = Icon(Icons.favorite);
 const IOS_ICO_NOFAV = Icon(Icons.favorite_border);
 const IOS_ICO_POPUP = Icon(Icons.more_vert);
-const IOS_TXT_POPUP_FAV = 'Only Favorites';
+const IOS_TXT_POPUP_FAV = 'Favorites';
 const IOS_TXT_POPUP_ALL = 'Show All';
 const IOS_ICO_FAV_NOTIF = Icon(Icons.notifications_active, color: Colors.white);
 
