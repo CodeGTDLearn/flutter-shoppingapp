06448f511ca3450f745825060b5de8aa55dc200f
PauloPortofolio <pvba02@gmail.com>
2021-11-12 16:27:31 -0500

    checkpoint GridAnimation

--- lib/app/modules/overview/view/overview_view.dart
+++ lib/app/modules/overview/view/overview_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 
 import '../../../core/components/app_messages_provided.dart';
@@ -12,28 +13,57 @@ import '../core/overview_widget_keys.dart';
 
 class OverviewView extends StatelessWidget {
   Widget build(BuildContext context) {
-    var controller = Get.find<OverviewController>();
-    controller.applyPopupFilter(EnumFilter.All);
+    var _controller = Get.find<OverviewController>();
+    _controller.applyPopupFilter(EnumFilter.All);
+
+    var _columnCount = 1;
+    var _gridLenght = 1;
 
     return Scaffold(
         key: K_OV_SCFLD_GLOB_KEY,
         appBar: CustomAppBar(filter: EnumFilter.All),
         drawer: Get.find<CustomDrawer>(),
-        body: Obx(() => controller.overviewViewGridViewItemsObs.isEmpty
-            ? SingleChildScrollView(
-                child: Center(
-                    child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                CustomIndicator.message(message: NO_PROD, fontSize: 20)
-              ])))
-            : GridView.builder(
-                padding: EdgeInsets.all(10),
-                itemCount: controller.overviewViewGridViewItemsObs.length,
-                itemBuilder: (_, item) => OverviewGridItem(
-                    controller.overviewViewGridViewItemsObs[item], item.toString()),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 3 / 2,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10))));
+        body: Obx(
+          () => _controller.overviewViewGridViewItemsObs.isEmpty
+              ? _overviewNoProductsInDb_info_singleChildScrollView()
+              : AnimationLimiter(
+                  child: GridView.count(
+                  crossAxisCount: _columnCount,
+                  children: List.generate(_gridLenght, (index) {
+                    return AnimationConfiguration.staggeredGrid(
+                        position: index,
+                        columnCount: _columnCount,
+                        child: ScaleAnimation(
+                          duration: Duration(milliseconds: 1000),
+                          child: FadeInAnimation(
+                              // child: _overviewGridItems_gridView(_controller),
+                              child: OverviewGridItem(
+                                  _controller.overviewViewGridViewItemsObs[index],
+                                  index.toString())),
+                        ));
+                  }),
+                )),
+        ));
+  }
+
+  GridView _overviewGridItems_gridView(OverviewController controller) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: controller.overviewViewGridViewItemsObs.length,
+        itemBuilder: (_, item) => OverviewGridItem(
+            controller.overviewViewGridViewItemsObs[item], item.toString()),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10));
+  }
+
+  SingleChildScrollView _overviewNoProductsInDb_info_singleChildScrollView() {
+    return SingleChildScrollView(
+        child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [CustomIndicator.message(message: NO_PROD, fontSize: 20)])));
   }
 }
